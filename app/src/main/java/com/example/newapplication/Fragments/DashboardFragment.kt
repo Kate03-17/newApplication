a1f94770bdc79998fea566ea0e048ef6f52d31bf
unknown <you@example.com>
2020-12-09 13:45:21 +0400

    original

---
+++ app/src/main/java/com/example/newapplication/Fragments/DashboardFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newapplication.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newapplication.R
+
+class DashboardFragment: Fragment(R.layout.fragment_dashboard) {
+}
---
+++ app/src/main/java/com/example/newapplication/Fragments/NotificationFragment.kt
@@ -0,0 +1,7 @@
+package com.example.newapplication.Fragments
+
+import androidx.fragment.app.Fragment
+import com.example.newapplication.R
+
+class NotificationFragment: Fragment(R.layout.fragment_notification) {
+}
